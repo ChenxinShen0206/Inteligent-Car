3768b6ec29a99984587ff7865b3fbd7d0dd5dfae
ChenxinShen0206 <ccx24@mails.tsinghua.edu.cn>
2025-09-08 21:21:16 +0800

    无Aurix工作环境文件的例程代码
    纯粹的解压包解压后的文件

--- XinDong_TC377TX-Example_PeriodicInterrupt/TriCore Debug (TASKING)/Libraries/iLLD/TC37AED/Tricore/_Impl/IfxScu_cfg.d
+++
@@ -1,8 +0,0 @@
-IfxScu_cfg.o :	../Libraries/iLLD/TC37AED/Tricore/_Impl/IfxScu_cfg.c
-../Libraries/iLLD/TC37AED/Tricore/_Impl/IfxScu_cfg.c :
-IfxScu_cfg.o :	..\Libraries\iLLD\TC37AED\Tricore\_Impl\IfxScu_cfg.h
-..\Libraries\iLLD\TC37AED\Tricore\_Impl\IfxScu_cfg.h :
-IfxScu_cfg.o :	"D:\\Tsinghua\\IntellectualRacingTeam\\XinDong2025\\Example\\XinDong_TC377TX-Example_PeriodicInterrupt\\Configurations\Ifx_Cfg.h"
-"D:\\Tsinghua\\IntellectualRacingTeam\\XinDong2025\\Example\\XinDong_TC377TX-Example_PeriodicInterrupt\\Configurations\Ifx_Cfg.h" :
-IfxScu_cfg.o :	"D:\\Tsinghua\\IntellectualRacingTeam\\XinDong2025\\Example\\XinDong_TC377TX-Example_PeriodicInterrupt\\Libraries\\Infra\\Sfr\\TC37AED\\_Reg\IfxScu_bf.h"
-"D:\\Tsinghua\\IntellectualRacingTeam\\XinDong2025\\Example\\XinDong_TC377TX-Example_PeriodicInterrupt\\Libraries\\Infra\\Sfr\\TC37AED\\_Reg\IfxScu_bf.h" :
